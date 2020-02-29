071f8de6a97c4e076044a0b8e1786f4beecf69cb
ailen munoz <ailenmunozz@gmail.com>
2020-02-29 16:33:38 -0300

    arreglos del linter

--- frontend/src/store/__tests__/reacciones.test.js
+++ frontend/src/store/__tests__/reacciones.test.js
@@ -1,10 +1,10 @@
-import reaccionesReducer, {reactionTypes} from "../reacciones";
+import reaccionesReducer, { reactionTypes } from '../reacciones';
 
 const unUsuario = {
   email: 'unEmail@email',
-  nombre: 'Un Email'
+  nombre: 'Un Email',
 };
-const reaccion = (reaccion, usuario) => ({nombre: reaccion, usuario});
+const reaccion = (reaccion, usuario) => ({ nombre: reaccion, usuario });
 
 describe('reaccionesReducer', () => {
   let state;
@@ -34,34 +34,33 @@ describe('reaccionesReducer', () => {
     });
 
     it('si el usuario no envio esa reaccion, la agrega', () => {
-      expect(reaccionesReducer(state, evento)).toEqual([reaccion('unaReaccion', unUsuario)])
+      expect(reaccionesReducer(state, evento)).toEqual([reaccion('unaReaccion', unUsuario)]);
     });
 
     it('si el usuario reacciona mas de una vez, no hace nada', () => {
       state = reaccionesReducer(state, evento);
-      expect(reaccionesReducer(state, evento)).toEqual([reaccion('unaReaccion', unUsuario)])
+      expect(reaccionesReducer(state, evento)).toEqual([reaccion('unaReaccion', unUsuario)]);
     });
   });
 
   describe(`#${reactionTypes.DESREACCIONAR}`, () => {
-
     beforeEach(() => {
       evento = {
         type: reactionTypes.DESREACCIONAR,
         usuario: unUsuario,
-        nombre: 'unaReaccion'
+        nombre: 'unaReaccion',
       };
       state = [];
     });
 
     it('si el usuario ya habia reaccionado, quita la reaccion', () => {
       state = [reaccion('unaReaccion', unUsuario)];
-      expect(reaccionesReducer(state, evento)).toEqual([])
+      expect(reaccionesReducer(state, evento)).toEqual([]);
     });
 
     it('si el usuario no habia reaccionado, no hace nada', () => {
-      state = [reaccion('unaReaccion', {nombre: 'alguien', email: 'otroEmail'})];
-      expect(reaccionesReducer(state, evento)).toEqual([reaccion('unaReaccion', {nombre: 'alguien', email: 'otroEmail'})])
+      state = [reaccion('unaReaccion', { nombre: 'alguien', email: 'otroEmail' })];
+      expect(reaccionesReducer(state, evento)).toEqual([reaccion('unaReaccion', { nombre: 'alguien', email: 'otroEmail' })]);
     });
-  })
+  });
 });
