ded82e03e462e377e55e083454a2c5e1bb2cdeb6
Lucas David Traverso <lucas6246@gmail.com>
2020-03-04 01:47:51 -0300

    Added ping/pong to keep connection alive for heroku

--- backend/src/webSocket.js
+++ backend/src/webSocket.js
@@ -8,8 +8,23 @@ function parserLastEvent(req) {
   return lastEventId;
 }
 
+function getCurrentTimestamp() {
+  return new Date().valueOf();
+}
+
 export default function (wss) {
   return async (ws, req) => {
+    const timeoutHandler = setInterval(() => {
+      ws.ping(getCurrentTimestamp().toString());
+    }, 20000)
+    ws.on('close', (code, reason) => {
+      clearInterval(timeoutHandler);
+      console.log(`closed connection (${code}) with reason: '${reason}'`)
+    });
+    ws.on('pong', (message) => {
+      const timeDelta = getCurrentTimestamp() - parseInt(message.toString(), 10);
+      console.log(`Pong took to respond: ${timeDelta}ms`);
+    });
     const lastEvent = parserLastEvent(req);
     const eventos = await context.eventosRepo.findEventosUltimaReunion(lastEvent);
     ws.send(JSON.stringify(
