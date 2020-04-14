a2bbff60b3f3a4143bd4509deafc3982e2f9581f
ailen munoz <ailenmunozz@gmail.com>
2020-04-14 20:27:55 -0300

    Calcula si tiene que desrreaccionar a un evento

--- frontend/src/store/__tests__/historicoDeReacciones.test.js
+++ frontend/src/store/__tests__/historicoDeReacciones.test.js
@@ -1,9 +1,14 @@
 import {reactionTypes} from "../reacciones";
 import historicoDeReacciones from '../historicoDeReacciones';
+import {reacciones} from "../../mobile";
 
 const unUsuario = {
   email: 'unEmail@email',
   nombre: 'Un Email',
+};
+const otroUsuario = {
+  email: 'otroEmaill@email',
+  nombre: 'Otro Email',
 };
 
 describe('historicoDeReaccionesReducer', () => {
@@ -35,6 +40,83 @@ describe('historicoDeReaccionesReducer', () => {
       expect(historicoDeReacciones(state, evento3)).toEqual([evento, evento2, evento3])
     });
 
+    describe(`Cuando hay reacciones de reacciones opuestas`, () => {
+      beforeEach(() => {
+        state = [];
+      });
+
+      it(`Si hay un ${reacciones.THUMBS_UP} y luego un ${reacciones.THUMBS_DOWN} del mismo usuario, deberia crear una desreaccion de ${reacciones.THUMBS_UP} `, () => {
+        const evento1 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '1', usuario: unUsuario};
+        const evento2 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '2', usuario: unUsuario};
+        const eventoAgregado = {type: reactionTypes.DESREACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '2', usuario: unUsuario};
+
+        state = historicoDeReacciones(state, evento1);
+        state = historicoDeReacciones(state, evento2);
+
+        expect(state).toEqual([evento1, evento2, eventoAgregado])
+      });
+
+      it(`Si hay un ${reacciones.THUMBS_DOWN} y luego un ${reacciones.THUMBS_UP} del mismo usuario, deberia crear una desreaccion de ${reacciones.THUMBS_DOWN} `, () => {
+        const evento1 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '1', usuario: unUsuario};
+        const evento2 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '2', usuario: unUsuario};
+        const eventoAgregado = {type: reactionTypes.DESREACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '2', usuario: unUsuario};
+
+        state = historicoDeReacciones(state, evento1);
+        state = historicoDeReacciones(state, evento2);
+
+        expect(state).toEqual([evento1, evento2, eventoAgregado])
+      });
+
+      it(`Si hay un ${reacciones.THUMBS_UP} y luego un ${reacciones.THUMBS_DOWN} de distintos usuarios, no deberia generarse ninguna desreaccion`, () => {
+        const evento1 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '1', usuario: unUsuario};
+        const evento2 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '2', usuario: otroUsuario};
+
+        state = historicoDeReacciones(state, evento1);
+        state = historicoDeReacciones(state, evento2);
+
+        expect(state).toEqual([evento1, evento2])
+      });
+
+      it(`Si hay un ${reacciones.THUMBS_DOWN} y luego un ${reacciones.THUMBS_UP} de distintos usuarios, no deberia generarse ninguna desreaccion`, () => {
+        const evento1 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '1', usuario: unUsuario};
+        const evento2 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '2', usuario: otroUsuario};
+
+        state = historicoDeReacciones(state, evento1);
+        state = historicoDeReacciones(state, evento2);
+
+        expect(state).toEqual([evento1, evento2])
+      });
+
+      it(`Si hay un ${reacciones.THUMBS_DOWN} que ya fue contrarrestado, y luego desreacciono de manera tradicional, no deberia generarse ningun evento nuevo`, () => {
+        const evento1 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '1', usuario: unUsuario};
+        const evento2 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '2', usuario: unUsuario};
+        const eventoAgregado = {type: reactionTypes.DESREACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '2', usuario: unUsuario};
+        const evento3 = {type: reactionTypes.DESREACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '3', usuario: unUsuario};
+        const evento4 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '4', usuario: unUsuario};
+
+        state = historicoDeReacciones(state, evento1);
+        state = historicoDeReacciones(state, evento2);
+        state = historicoDeReacciones(state, evento3);
+        state = historicoDeReacciones(state, evento4);
+
+        expect(state).toEqual([evento1, evento2, eventoAgregado, evento3, evento4])
+      });
+
+      it(`Si hay un ${reacciones.THUMBS_UP} que ya fue contrarrestado, no deberia volver a generarse su desrreaccion`, () => {
+        const evento1 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '1', usuario: unUsuario};
+        const evento2 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '2', usuario: unUsuario};
+        const eventoAgregado = {type: reactionTypes.DESREACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '2', usuario: unUsuario};
+        const evento3 = {type: reactionTypes.REACCIONAR, nombre: reacciones.THUMBS_UP, fecha: '3', usuario: unUsuario};
+        const eventoAgregado2 = {type: reactionTypes.DESREACCIONAR, nombre: reacciones.THUMBS_DOWN, fecha: '3', usuario: unUsuario};
+
+        state = historicoDeReacciones(state, evento1);
+        state = historicoDeReacciones(state, evento2);
+        state = historicoDeReacciones(state, evento3);
+
+        expect(state).toEqual([evento1, evento2, eventoAgregado, evento3, eventoAgregado2])
+      });
+    });
+
   });
 
   describe(`${reactionTypes.DESREACCIONAR}`, () => {
@@ -63,6 +145,7 @@ describe('historicoDeReaccionesReducer', () => {
     });
 
   });
+
   describe(`Cualquier otro evento`, () => {
 
     beforeEach(() => {
--- frontend/src/store/historicoDeReacciones.js
+++ frontend/src/store/historicoDeReacciones.js
@@ -1,9 +1,60 @@
 import {produce} from "immer";
 import {reactionTypes} from "./reacciones";
+import {reacciones} from "../mobile";
+
+function elNombreDelEventoEs(evento, nombreEvento) {
+  return evento.nombre === nombreEvento;
+}
+
+function elUsuarioYaRealizoLaReaccionContraria(draft, usuario, reaccionContraria) {
+  return draft.some(event => event.usuario.email === usuario.email && event.nombre === reaccionContraria);
+}
+
+function peroYaFueContrarrestada(draft, usuario, reaccionActual, reaccionContraria) {
+  /*
+  *  Estoy super quemada pero deberia funcionar.
+  *  El caso que contempla es si:
+  *  +1 Reaccion
+  *  -1 Reaccion
+  *  +1 Desrreaccion (lo genera el reductor)
+  *  -1 Desrreaccion (por el usuario. En este caso queda en "neutro")
+  *  +1 Reaccion => Esta deberia sumarla, no contrarrestarla.
+  * */
+  const reaccionesAlEvento = draft.filter(event => event.usuario.email === usuario.email && event.nombre === reaccionActual && event.type === reactionTypes.REACCIONAR);
+  const desrreaccionesAlEvento = draft.filter(event => event.usuario.email === usuario.email && event.nombre === reaccionActual && event.type === reactionTypes.DESREACCIONAR);
+
+  const reaccionesContrariasAlEvento = draft.filter(event => event.usuario.email === usuario.email && event.nombre === reaccionContraria && event.type === reactionTypes.REACCIONAR);
+  const desrreaccionesContrariasAlEvento = draft.filter(event => event.usuario.email === usuario.email && event.nombre === reaccionContraria && event.type === reactionTypes.DESREACCIONAR);
+
+  return (reaccionesAlEvento.length - 1 === desrreaccionesAlEvento.length) && (reaccionesContrariasAlEvento.length === desrreaccionesContrariasAlEvento.length);
+}
 
 const historicoDeReaccionesReducer = (state = [], evento) => produce(state, (draft) => {
-  if(evento.type === reactionTypes.REACCIONAR || evento.type === reactionTypes.DESREACCIONAR) draft.push(evento);
-  return draft;
+  switch(evento.type){
+    case reactionTypes.REACCIONAR:
+      draft.push(evento); // agrego el nuevo evento, siempre
+
+      const {usuario} = evento;
+      if(elNombreDelEventoEs(evento, reacciones.THUMBS_UP)
+        && elUsuarioYaRealizoLaReaccionContraria(draft, usuario, reacciones.THUMBS_DOWN) &&
+        !peroYaFueContrarrestada(draft, usuario, reacciones.THUMBS_UP, reacciones.THUMBS_DOWN)
+      ){
+        draft.push({...evento, type: reactionTypes.DESREACCIONAR, nombre: reacciones.THUMBS_DOWN});
+      }
+
+      if(elNombreDelEventoEs(evento, reacciones.THUMBS_DOWN)
+        && elUsuarioYaRealizoLaReaccionContraria(draft, usuario, reacciones.THUMBS_UP) &&
+        !peroYaFueContrarrestada(draft, usuario, reacciones.THUMBS_DOWN, reacciones.THUMBS_UP)
+      ){
+          draft.push({...evento, type: reactionTypes.DESREACCIONAR, nombre: reacciones.THUMBS_UP});
+  }
+      break;
+    case reactionTypes.DESREACCIONAR:
+      draft.push(evento);
+      break;
+    default:
+      return draft;
+  }
 });
 
 export default historicoDeReaccionesReducer;
