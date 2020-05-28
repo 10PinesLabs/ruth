047b21335f21e146e59c66f29202dd3411723382
Lucas David Traverso <lucas6246@gmail.com>
2020-05-28 13:09:29 -0300

    Fix secure cookies issue when running in heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,22 +9,30 @@ import logger from '~/logger';
 const app = express();
 require('express-ws')(app);
 
-app.use(morgan('combined', { stream: logger.stream }));
-app.use(json());
-app.use(urlencoded({ extended: false }));
-
 const cookieOptions = {
   name: 'ruth_session',
   maxAge: 24 * 60 * 60 * 1000, // 24 hours
 };
 
 if (process.env.NODE_ENV === 'production') {
-  cookieOptions.secure = true;
   cookieOptions.secret = process.env.SESSION_SECRET;
+  // This is necessary since the app is not server directly into the internet
+  // but is behind a proxy (heroku) so the machinery that express uses to check
+  // if the request is secure isn't very useful (requests are secure when they
+  // reach the router but the connection between the router and express is not
+  //
+  // To aliviate this we should trust the X-Forward-* family of headers
+  // more on this https://expressjs.com/en/guide/behind-proxies.html
+  app.set('trust proxy', true);
+  cookieOptions.secure = true;
 } else {
   cookieOptions.secure = false;
   cookieOptions.secret = process.env.SESSION_SECRET || 'secret';
 }
+
+app.use(morgan('combined', { stream: logger.stream }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
 app.use(cookieSession(cookieOptions));
 
