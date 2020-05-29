f701c39395ab3ed0925011690e45cd1708b588e2
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-05-29 17:23:40 -0300

    Añadida la pagina de minuta que muestra el timer y el titulo del tema

--- frontend/src/tipos-vista-principal/Minuta.js
+++ frontend/src/tipos-vista-principal/Minuta.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import {GraphsContainer, SubDebateContainer} from "../debate-handler/Debate.styled";
-import ParticipantsQueue from "../cola-de-participantes/ParticipantsQueue";
-import ChartBar from "../chart/chartBar";
-import ChartLine from "../chart/chartLine";
+import React, {useState} from 'react';
+import {faCaretLeft, faCaretRight} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Botonera, BotoneraNavegacionTemas, VistaDelMedioContainer,} from './Resumen.styled';
+import InfoTema from '../temario/InfoTema';
+import {Button, SecondaryButton} from '../components/Button.styled';
 import {useSpring} from "react-spring";
+import DescripcionTemaComun from "../temario/descripcion-tipo-tema/DescripcionTemaComun";
+import DescripcionActionItems from "../temario/descripcion-tipo-tema/DescripcionActionItems";
+import DescripcionPropuestaPinos from "../temario/descripcion-tipo-tema/DescripcionPropuestaPinos";
+import {ModalDeConfirmacion} from "./Modal";
+import Zoom from "@material-ui/core/Zoom";
 
-const Minuta = ({tema}) => {
-    const debateData = {
-        participants: tema.oradores,
-        dataBar: {
-            data: tema.reacciones,
-        },
-        dataLine: {
-            data: tema.historicoDeReacciones,
-        },
-    };
-    const props = useSpring({opacity: 1, from: {opacity: 0}});
-    return (
-        <SubDebateContainer style={props}>
-            <GraphsContainer>
-                <ChartLine data={debateData.dataLine} inicioTema={tema.inicio}/>
-                <ChartBar data={debateData.dataBar}/>
-            </GraphsContainer>
-            <ParticipantsQueue participants={debateData.participants} finTema={tema.fin}/>
-        </SubDebateContainer>
-    );
+const tiposDeTema = {
+    'conDescripcion': DescripcionTemaComun,
+    'repasarActionItems': DescripcionActionItems,
+    'proponerPinos': DescripcionPropuestaPinos,
 };
 
+const Minuta = ({tema, retrocederTema, temaATratar, empezarTema, avanzarTema, temaActivo, terminarTema}) => {
+
+    const props = useSpring({opacity: 1, from: {opacity: 0}});
+
+    return (
+        <VistaDelMedioContainer style={props}>
+          aca va a ir toda la logica de la pagina :D
+        </VistaDelMedioContainer>
+    );
+};
 export default Minuta;
