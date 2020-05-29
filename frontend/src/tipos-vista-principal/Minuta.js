c194ab6b6b1502bec4badf94adba08d905d2255a
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-05-29 18:03:25 -0300

    Optimizados los imports!

--- frontend/src/tipos-vista-principal/Minuta.js
+++ frontend/src/tipos-vista-principal/Minuta.js
@@ -1,21 +1,7 @@
-import React, {useState} from 'react';
-import {faCaretLeft, faCaretRight} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Botonera, BotoneraNavegacionTemas, VistaDelMedioContainer,} from './Resumen.styled';
-import InfoTema from '../temario/InfoTema';
-import {Button, SecondaryButton} from '../components/Button.styled';
+import React from 'react';
+import {VistaDelMedioContainer,} from './Resumen.styled';
 import {useSpring} from "react-spring";
-import DescripcionTemaComun from "../temario/descripcion-tipo-tema/DescripcionTemaComun";
-import DescripcionActionItems from "../temario/descripcion-tipo-tema/DescripcionActionItems";
-import DescripcionPropuestaPinos from "../temario/descripcion-tipo-tema/DescripcionPropuestaPinos";
-import {ModalDeConfirmacion} from "./Modal";
-import Zoom from "@material-ui/core/Zoom";
 
-const tiposDeTema = {
-    'conDescripcion': DescripcionTemaComun,
-    'repasarActionItems': DescripcionActionItems,
-    'proponerPinos': DescripcionPropuestaPinos,
-};
 
 const Minuta = ({tema, retrocederTema, temaATratar, empezarTema, avanzarTema, temaActivo, terminarTema}) => {
 
