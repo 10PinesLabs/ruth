d6039245c413e69bb64ba0ae523ac14362fd1ba9
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-06-01 13:10:38 -0300

    Los botones ahora se ven a la izquierda de la imagen!

--- frontend/src/cola-de-participantes/ParticipantsCard.js
+++ frontend/src/cola-de-participantes/ParticipantsCard.js
@@ -13,7 +13,38 @@ import {reacciones} from "../mobile/actions";
 import {ReactionButton} from "../mobile/ReactionButton";
 import Grid from "@material-ui/core/Grid";
 import {green, yellow} from "@material-ui/core/colors";
+import * as PropTypes from "prop-types";
 
+function TalkingReactions(props) {
+  return <Grid container direction="column" spacing={3} style={{width: "30%",alignItems: "center",alignSelf:"flex-start"}}>
+    <Grid item xs={4} justify="center" alignItems="center" style={props.style}>
+      <ReactionButton
+          background={props.background}
+          isBig
+          isActive={true} icon={faThumbsUp}
+          onClick={props.onClick}/>
+    </Grid>
+    <Grid item xs={4} justify="center" alignItems="center" style={props.style}>
+      <ReactionButton
+          background={props.background}
+          isBig
+          isActive={true} icon={faThumbsDown}
+          onClick={props.onClick}/>
+    </Grid>
+    <Grid item xs={4} justify="center" alignItems="center" style={props.style}>
+      <ReactionButton
+          background={props.background}
+          isBig
+          isActive={true} icon={faHashtag}
+          onClick={props.onClick}/>
+    </Grid>
+  </Grid>;
+}
+TalkingReactions.propTypes = {
+  style: PropTypes.shape({display: PropTypes.string, paddingRight: PropTypes.number, paddingLeft: PropTypes.number}),
+  background: PropTypes.string,
+  onClick: PropTypes.func
+};
 const ParticipantsCard = ({participant, isParticipantTalking, interactive, kickear, finTema}) => {
   const estadoOrador = () => {
     if (estaEncolado()) {
@@ -47,44 +78,30 @@ const ParticipantsCard = ({participant, isParticipantTalking, interactive, kicke
   let centerReactionBtn = {
     display: "flex",
     paddingLeft: 0,
-    paddingRight: 0
+    paddingRight: 0,
+    maxWidth: "none"
   };
 
   let reactionColor = 'linear-gradient(90deg, rgba(220,223,3,1) 0%, rgba(255,252,184,1) 100%)';
   return showSkeleton ? <SkeletonComponent interactive isParticipantTalking={isParticipantTalking}/> : (participant ? (
-    <CardContainer isInteractive={interactive} isTalking={isParticipantTalking}>
-      {interactive && <Cerrar onClick={() => setOradorAKickear(participant.usuario)}/>}
-      <ModalDeConfirmacion title={`¿Estás seguro que querés kickear a ${oradorAKickear && oradorAKickear.nombre || ''}?`}
-                           open={Boolean(oradorAKickear)}
-                           confirmText={"Si"}
-                           cancelText={"No"}
-                           onClose={() => setOradorAKickear(null)} onConfirm={() => kickear(oradorAKickear)}/>
-      <UserAvatar isTalking={isParticipantTalking} avatar={getGravatarUrlFor(participant.usuario.email)}/>
-      <CardInfoContainer>
-        <CardName isInteractive={interactive}> {participant.usuario.nombre} </CardName>
-        <ParticipantCounter isInteractive={interactive} estadoOrador={estadoOrador()}/>
-        <Grid container spacing={3}>
-          <Grid item xs={4} justify="center" alignItems="center" style={centerReactionBtn}>
-            <ReactionButton
-                background={reactionColor}
-                isActive={true} icon={faThumbsUp}
-                onClick={() => console.log("lol")}/>
-          </Grid>
-          <Grid item xs={4} justify="center" alignItems="center" style={centerReactionBtn}>
-            <ReactionButton
-                background={reactionColor}
-                isActive={true} icon={faThumbsDown}
-                onClick={() => console.log("lol")}/>
-          </Grid>
-          <Grid item xs={4} justify="center" alignItems="center" style={centerReactionBtn}>
-            <ReactionButton
-                background={reactionColor}
-                isActive={true} icon={faHashtag}
-                onClick={() => console.log("lol")}/>
-          </Grid>
-        </Grid>
-      </CardInfoContainer>
-    </CardContainer>
+
+      <CardContainer style={{flexDirection: "row", width: "70%"}} isInteractive={interactive} isTalking={isParticipantTalking}>
+        {interactive && <Cerrar onClick={() => setOradorAKickear(participant.usuario)}/>}
+        <ModalDeConfirmacion
+            title={`¿Estás seguro que querés kickear a ${oradorAKickear && oradorAKickear.nombre || ''}?`}
+            open={Boolean(oradorAKickear)}
+            confirmText={"Si"}
+            cancelText={"No"}
+            onClose={() => setOradorAKickear(null)} onConfirm={() => kickear(oradorAKickear)}/>
+        <TalkingReactions style={centerReactionBtn} background={reactionColor} onClick={() => console.log("lol")}/>
+        <div style={{width: "70%"}}>
+          <UserAvatar isTalking={isParticipantTalking} avatar={getGravatarUrlFor(participant.usuario.email)}/>
+          <CardInfoContainer>
+            <CardName isInteractive={interactive}> {participant.usuario.nombre} </CardName>
+            <ParticipantCounter isInteractive={interactive} estadoOrador={estadoOrador()}/>
+          </CardInfoContainer>
+        </div>
+      </CardContainer>
   ) : <div> Nadie esta hablando</div>);
 };
 
