82a5a9b369050046d6ebf742521c6b8d729414c2
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-06-05 16:47:43 -0300

    Merge Con master

--- frontend/src/tipos-vista-principal/Minuta.js
+++ frontend/src/tipos-vista-principal/Minuta.js
@@ -64,7 +64,7 @@ const Minuta = ({ dispatch, tema, temaActivo }) => {
           onChange={(event) => {
             userChangedConclusionInput(event.target.value);
           }}
-        ></textarea>
+        />
 
         {isEditingConclusion ? (
           <div>
