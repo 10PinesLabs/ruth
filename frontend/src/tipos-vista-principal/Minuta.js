77682f6319bf28507e73d20daf489355a76fb5b3
amuroBosetti <mauro.bosetti@10pines.com>
2020-06-08 12:01:38 -0300

    Agregado placeholder y boton con collapse. Agregado lo necesario para implementar que el contenedor del resumen de la persona que este hablando pueda colapsar y reaparecer con un boton

--- frontend/src/tipos-vista-principal/Minuta.js
+++ frontend/src/tipos-vista-principal/Minuta.js
@@ -1,17 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { VistaDelMedioContainer } from "./Resumen.styled";
-import { useSpring } from "react-spring";
-import { connect } from "react-redux";
-import { tipoDeEvento, conclusionReducer } from "../store/conclusion";
-import { toast } from "react-toastify";
-import { Button, SecondaryButton } from "../components/Button.styled";
+import React, {useState, useEffect} from "react";
+import {VistaDelMedioContainer} from "./Resumen.styled";
+import {useSpring} from "react-spring";
+import {connect} from "react-redux";
+import {tipoDeEvento, conclusionReducer} from "../store/conclusion";
+import {toast} from "react-toastify";
+import {Button, SecondaryButton} from "../components/Button.styled";
 import ListaPinosQueHablaron from "../minuta/ListaPinosQueHablaron";
+import {Card, CardContent} from "@material-ui/core";
+import Collapse from "@material-ui/core/Collapse";
 
 
-const Minuta = ({ dispatch, tema, temaActivo }) => {
+const Minuta = ({dispatch, tema, temaActivo}) => {
   let [lastKnowConclusion, setLastKnowConclusion] = useState(tema.conclusion);
   let [conclusion, setConclusion] = useState(tema.conclusion);
   let [isEditingConclusion, setIsEditingConclusion] = useState(false);
+  const [isRecapCollapsed, setIsRecapCollapsed] = useState(true);
 
   const dispatchMinuta = (data) => {
     const evento = {
@@ -55,9 +58,19 @@ const Minuta = ({ dispatch, tema, temaActivo }) => {
 
   return (
     <VistaDelMedioContainer
-      style={useSpring({ opacity: 1, from: { opacity: 0 } })}
+      style={useSpring({opacity: 1, from: {opacity: 0}})}
     >
+      <button onClick={() => setIsRecapCollapsed(!isRecapCollapsed)}>Desplegar</button>
+      <Collapse in={isRecapCollapsed}>
+        <Card>
+          <CardContent>
+            Aca va a ir lo de la conclusion
+          </CardContent>
+        </Card>
+      </Collapse>
+
       <ListaPinosQueHablaron oradores={tema.oradores}/>
+
       <form>
         <textarea
           value={conclusion}
