48af6e52a3770a0c848032669181dd11c72fd1e4
Mauro Bosetti <46928545+amurobosetti@users.noreply.github.com>
2020-06-09 13:28:03 -0300

    Corregido typo

--- frontend/src/minuta/ListaPinosQueHablaron.js
+++ frontend/src/minuta/ListaPinosQueHablaron.js
@@ -7,6 +7,25 @@ export const cantidadReaccionesDelPino = (tipoReaccion,pino) => {
   return pino.reacciones[tipoReaccion].length;
 }
 
+const generarEstadoPara = (pino, finTema) => {
+  debugger
+  if (estaEncolado(pino)) {
+    return {detalle: 'encolado'};
+  }
+  if (hablo(pino)) {
+    return {detalle: 'hablo', seconds: Math.ceil((pino.fin - pino.inicio) / 1000)};
+  }
+  return {detalle: finTema? 'hablo' : 'hablando', seconds: Math.ceil(((Date.parse(finTema) || Date.now()) - pino.inicio) / 1000)};
+};
+
+const hablo = (pino) => {
+  return pino.fin !== null;
+}
+
+const estaEncolado = (pino) => {
+  return pino.inicio === null;
+};
+
 const FilaTitulos = () => {
   return <FilaTitulosWrapper>
     <th>
@@ -32,23 +51,6 @@ const FilaTitulos = () => {
     </th>
   </FilaTitulosWrapper>;
 };
-const generarEstadoPara = (pino, finTema) => {
-  if (estaEncolado(pino)) {
-    return {detalle: 'encolado'};
-  }
-  if (hablo(pino)) {
-    return {detalle: 'hablo', seconds: Math.ceil((pino.fin - pino.inicio) / 1000)};
-  }
-  return {detalle: finTema? 'hablo' : 'hablando', seconds: Math.ceil(((Date.parse(finTema) || Date.now()) - pino.inicio) / 1000)};
-};
-
-const hablo = (pino) => {
-  return pino.fin !== null;
-}
-
-const estaEncolado = (pino) => {
-  return pino.inicio === null;
-};
 
 const FilaPino = (props) => <tr>
   <td>
@@ -58,7 +60,7 @@ const FilaPino = (props) => <tr>
     {props.pino.usuario.nombre}
   </Td>
   <Td>
-    <ParticipantCounter estadoOrador={generarEstadoPara(props.pino, props.tema.finTema)}/>
+    <ParticipantCounter estadoOrador={generarEstadoPara(props.pino, props.tema.fin)}/>
   </Td>
   <Td>
     {cantidadReaccionesDelPino(TiposReaccionAlHablar.THUMBS_UP,props.pino)}
