2ad12b94d6de43c2ff250f34cc5d868ef0e0f9c9
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-06-16 11:45:08 -0300

    se cambia estilo de la lista a tarjet y se agregan los divisores

--- frontend/src/minuta/ListaActionItems.js
+++ frontend/src/minuta/ListaActionItems.js
@@ -2,30 +2,45 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import Divider from '@material-ui/core/Divider';
+
 
 import {ListaActionItemsContainer, Titulo} from '../minuta/ListaActionItems.styled'
 
-const ActionItem = () =>{
+const actionItem = (action) =>{
     return (
         <>
-        <ListItem>Item lol</ListItem>
-        <hr/>
+        <ListItem>{action}</ListItem>
+
         </>
     )
 }
 
+const items = [
+    "Como minuteador, quiero poder enviar la minuta por email al finalizar la reunion de roots",
+    "Como owner de un action item, me gustaria recibir un email con la descripcion del mismo",
+    "[BUG] No se puede iniciar sesion en produccion"
+]
+
+const actionItemsConDivisores = (actionItems) => {
+    const itemsConDivisores = []
+    actionItems.forEach((item, index) => {
+        itemsConDivisores.push(actionItem(item))
+        if(items[index+1]) itemsConDivisores.push(<Divider/>)
+    })
+    return itemsConDivisores
+}
 
 export const ListaActionItems = () => {
-
+  
+    const itemsConDivisores = []
     return (
         <ListaActionItemsContainer>
-            <Titulo>Action Items</Titulo>
+            <Titulo>Action Items (##)</Titulo>
             <Button variant="outlined"> + Agregar Action Item</Button>
-            <List alignItems="flex-start">
-                <ActionItem/>
-                <ActionItem/>
-                <ActionItem/>
-                <ActionItem/>
+            <List alignItems="flex-start" component={Card}>
+                {actionItemsConDivisores(items)}
             </List>
             
         </ListaActionItemsContainer>
