efebf92e4458888bb16648e2b0584b4f9c6bf48a
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-06-18 13:30:36 -0300

    Merge con lo que hay en el branch de la lista

--- frontend/src/minuta/ListaActionItems.js
+++ frontend/src/minuta/ListaActionItems.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Card from '@material-ui/core/Card';
@@ -10,7 +10,7 @@ import {ListaActionItemsContainer,
         Titulo, 
         Owner} from '../minuta/ListaActionItems.styled'
 
-const actionItem = (descripcion, owners) =>{
+const ActionItem = ({descripcion, owners, seEstaEditando}) => {
   let [hoveringItem, setHoveringItem] = useState(false);
   const itemStyle = { backgroundColor: seEstaEditando ? colors.primary : colors.background,
     cursor: hoveringItem ? 'pointer' : 'auto',}
@@ -30,7 +30,7 @@ const actionItem = (descripcion, owners) =>{
 const actionItemsConDivisores = (actionItems) => {
     const itemsConDivisores = []
     actionItems.forEach((item, index) => {
-        itemsConDivisores.push(actionItem(item.actionItem.descripcion, item.actionItem.owners))
+        itemsConDivisores.push(<ActionItem descripcion={item.actionItem.descripcion} owners={item.actionItem.owners}/>)
         if(actionItems[index+1]) itemsConDivisores.push(<Divider/>)
     })
     return itemsConDivisores
