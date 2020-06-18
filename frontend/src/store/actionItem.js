7e5879eb07b9ffe244133fcab25489b07acd6269
amuroBosetti <mauro.bosetti@10pines.com>
2020-06-18 13:42:35 -0300

    Eliminada verificacion innecesaria del estado inicial del reducer

--- frontend/src/store/actionItem.js
+++ frontend/src/store/actionItem.js
@@ -9,10 +9,6 @@ const INITIAL_ACTION_ITEMS_STATE = [];
 export const actionItemReducer = (state = INITIAL_ACTION_ITEMS_STATE, evento) => produce(state, (prevActionItems) => {
   switch (evento.type) {
     case (tipoDeEvento.AGREGAR_ACTION_ITEM):
-      if (prevActionItems){
-        return [...prevActionItems, evento];
-      } else {
-        return [evento];
-      }
+      return [...prevActionItems, evento];
   }
 });
