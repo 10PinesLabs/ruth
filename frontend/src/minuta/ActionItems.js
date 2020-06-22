0e45f86d3debb0b288be8d33b38dfdf516778ca1
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-06-22 11:16:01 -0300

    Fragment eliminado cuando no era necesario

--- frontend/src/minuta/ActionItems.js
+++ frontend/src/minuta/ActionItems.js
@@ -23,36 +23,34 @@ const ActionItems = ({onAgregarActionItem}) => {
   }
 
   return (
-    <>
-      <Box component={ContenedorEdicionActionItem} clone boxShadow={2}>
-        <ContenedorEdicionActionItem maxWidth="md">
-          <ContenedorInputActionItem maxWidth="md">
-            <InputActionItem value={descripcion} onChange={(event) => setDescripcion(event.target.value)} multiline label="Descripcion"/>
-            <Autocomplete
-              multiple
-              options={listaDeRoots}
-              value={owners}
-              onChange={(event, value) => setOwners(value)}
-              renderInput={params => (
-                <TextField {...params}
-                placeholder="Owners"
-                margin="normal"
-                fullWidth/>
-              )
-              }/>
-          </ContenedorInputActionItem>
-          <ContenedorBotonesActionItem>
-            <BotonCancelar size="small" onClick={limpiarInputs} variant="outlined">Descartar</BotonCancelar>
-            <BotonCrearActionItem
-              size="small"
-              onClick={() => {
-                onAgregarActionItem({descripcion, owners});
-                limpiarInputs();
-              }}>Crear action item</BotonCrearActionItem>
-          </ContenedorBotonesActionItem>
-        </ContenedorEdicionActionItem>
-      </Box>
-    </>
+    <Box component={ContenedorEdicionActionItem} clone boxShadow={2}>
+      <ContenedorEdicionActionItem maxWidth="md">
+        <ContenedorInputActionItem maxWidth="md">
+          <InputActionItem value={descripcion} onChange={(event) => setDescripcion(event.target.value)} multiline label="Descripcion"/>
+          <Autocomplete
+            multiple
+            options={listaDeRoots}
+            value={owners}
+            onChange={(event, value) => setOwners(value)}
+            renderInput={params => (
+              <TextField {...params}
+              placeholder="Owners"
+              margin="normal"
+              fullWidth/>
+            )
+            }/>
+        </ContenedorInputActionItem>
+        <ContenedorBotonesActionItem>
+          <BotonCancelar size="small" onClick={limpiarInputs} variant="outlined">Descartar</BotonCancelar>
+          <BotonCrearActionItem
+            size="small"
+            onClick={() => {
+              onAgregarActionItem({descripcion, owners});
+              limpiarInputs();
+            }}>Crear action item</BotonCrearActionItem>
+        </ContenedorBotonesActionItem>
+      </ContenedorEdicionActionItem>
+    </Box>
   )
 };
 
