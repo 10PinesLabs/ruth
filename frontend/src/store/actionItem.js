ef9df46cd8783e24e65d6548dbefd948a35839ce
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-06-23 10:00:47 -0300

    Modificado el actionItem reducer para que en vez de retornar simplemente modifique el draft.

--- frontend/src/store/actionItem.js
+++ frontend/src/store/actionItem.js
@@ -10,11 +10,12 @@ const INITIAL_ACTION_ITEMS_STATE = [];
 export const actionItemReducer = (state = INITIAL_ACTION_ITEMS_STATE, evento) => produce(state, (prevActionItems) => {
   switch (evento.type) {
     case (tipoDeEvento.AGREGAR_ACTION_ITEM):
-      return [...prevActionItems, evento];
+      prevActionItems.push(evento);
+      break;
     case (tipoDeEvento.EDITAR_ACTION_ITEM):
       if(prevActionItems[evento.actionItem.index]){
         prevActionItems[evento.actionItem.index] = evento
-        return prevActionItems;
       }
+      break;
   }
 });
