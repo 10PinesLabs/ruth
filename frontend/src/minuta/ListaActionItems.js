4c113fc71670d27bc9c23dbe070fa7b337dab5a9
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-06-24 18:40:37 -0300

    Fix ahora los action items se crean en orden descendiente!

--- frontend/src/minuta/ListaActionItems.js
+++ frontend/src/minuta/ListaActionItems.js
@@ -6,11 +6,11 @@ import Divider from '@material-ui/core/Divider';
 import {ActionItemContainer, ActionItemDescription, ListaActionItemsContainer, Owner} from './ListaActionItems.styled'
 import {ActionItemEditor} from "./ActionItemEditor";
 
-const ActionItem = ({descripcion, owners, onEdit, index}) =>{
+const ActionItem = ({descripcion, owners, onEdit, id}) =>{
   const [estaEditando, setEstaEditando] = useState(false);
 
   const alGuardarEdicion = (actionItemGuardado) => {
-    onEdit({...actionItemGuardado, index})
+    onEdit({...actionItemGuardado, id})
     setEstaEditando(false)
   }
 
@@ -53,11 +53,11 @@ export const ListaActionItems = ({actionItems, onEdit}) => {
   return (
     <ListaActionItemsContainer>
         <List alignItems="flex-start" component={Card}>
-            {actionItems.map((item, index) =>
+            {actionItems.slice().reverse().map((item, index) =>
               <>
                 <ActionItem 
                   key={item.id} 
-                  index={index}
+                  id={item.id}
                   descripcion={item.actionItem.descripcion} 
                   owners={item.actionItem.owners}
                   onEdit={onEdit}
