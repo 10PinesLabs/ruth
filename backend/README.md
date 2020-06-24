ac7cff6c48e56c34829d5625cbb30ce3fd9633d6
amuroBosetti <mauro.bosetti@10pines.com>
2020-06-24 09:47:57 -0300

    Agrega envio de mail cuando se finaliza una reunion

--- backend/README.md
+++ backend/README.md
@@ -15,6 +15,28 @@ Como tenemos `sequalize-cli` podemos crear modelos _a la rails_:
 ### Base de datos en docker
 Si tienen docker, pueden levantar la base de datos haciendo: `$ dcompose up -d --no-recreate`.
 
+#### Mails
+#####Produccion
+En produccion, las variables de ambiente relacionadas a los mails deben quedar de esta manera:
+```
+MAIL_HOST=smtp.gmail.com,
+MAIL_PORT=465,
+```
+Ademas de las variables relacionadas a usuario, contraseña, destinatarios, que tendras que pedirlas en el channel de #ruth
+
+#####Local
+En local, para poder verificar como se estan enviando de mails, podes settear las variables de ambiente del backend siguiendo el .example, utilizando una direccion de correo de Ethereal Mail:
+1. Ir a `https://ethereal.email/` y crear una cuenta (lleva un solo click)
+1. Settear las variables de entorno de la siguiente forma:
+```
+MAIL_HOST=smtp.ethereal.email,
+MAIL_PORT=587,
+MAIL_USER= (el dado por Ethereal)
+MAIL_PASS= (el dado por Ethereal)
+```
+Tanto el destino (`MAIL_DESTINATION`) como la direccion de envio (`MAIL_SENDER_ADRESS`) son indistintas, porque todo es capturado por Ethereal.
+1. Al cerrar una reunion, lo veras en la cuenta de Ethereal ingresada
+
 ### mensajes
 Si se setea MESSAGE_TESTING a true los mensajes de slack seran redirigidos a #ruth-dev, si no se enviaran como dm
 ### TODO
---
+++ backend/src/domain/mail/mailParser.js
@@ -0,0 +1,94 @@
+const ejs = require('ejs');
+
+const componerMailResumen = (reunion, temas, fecha) => {
+  const fueTratado = (tema) => tema.inicio != null;
+  const temasAListar = temas.filter((tema) => fueTratado(tema));
+
+  const emojisReacciones = {
+    'Thumbs up': 128077,
+    'Thumbs down': 128078,
+    'Seguirlo por slack': 128260,
+    Redondear: 128260,
+  };
+
+  const posiblesReaccionesATema = [
+    'Thumbs up',
+    'Thumbs down',
+    'Seguirlo por slack',
+    'Redondear',
+  ];
+
+  const cantidadReacciones = (stringReaccion, reacciones) => {
+    const emoji = String.fromCodePoint(emojisReacciones[stringReaccion]);
+    if (emoji in reacciones) {
+      return reacciones[emoji].length;
+    }
+    return 0;
+  };
+
+  const timeStampElapsed = (fin, inicio) => Math.ceil((fin - inicio) / 1000);
+
+  const minutosHablando = (orador) => Math
+    .floor(Math.abs(timeStampElapsed(orador.fin, orador.inicio)) / 60);
+
+  const segundosHablando = (orador) => (`0${Math.abs(timeStampElapsed(orador.fin, orador.inicio)) % 60}`).slice(-2);
+
+  const tiempoHablando = (orador) => (`${minutosHablando(orador)}:${segundosHablando(orador)}`);
+
+  const reaccionesATema = (tema) => (
+    posiblesReaccionesATema.map((reaccion) => ({
+      reaccion,
+      cantidad: cantidadReacciones(reaccion, tema.reacciones),
+    })));
+
+  return (
+    ejs.render(`
+      <h1>Reunion <%= fecha %></h1>
+      <p>Temas:</p> 
+      <ul> 
+        <% temas.forEach((tema) => { %>
+            <li> <%= tema.titulo %> </li>
+        <% }) %> 
+      </ul>
+      <% temasAListar.forEach((tema) => { %>
+        <h2> <%= tema.titulo %> </h2>
+        <p> Descripcion: <%= tema.descripcion %> </p>
+        <p> Conclusion: <%= tema.conclusion %> </p>
+        <p> Reacciones: </p>
+        <ul>
+            <% reaccionesATema(tema).forEach(({reaccion, cantidad}) => { %>
+                <li> <%= reaccion %> : <%= cantidad %></li>
+            <% }) %>
+        </ul>
+        <p>Debate:</p>
+        <ul>
+            <% tema.oradores.pasados.forEach((orador) => { %>
+                <li> 
+                    <p><%= orador.usuario.nombre %> (<%= tiempoHablando(orador) %>)</p>
+                    <p>Resumen: <%= orador.resumen %></p>
+                    <ul>
+                          <li> Thumbs up: <%= orador.reacciones.thumbsUp.length %> </li>
+                          <li> Thumbs down: <%= orador.reacciones.thumbsDown.length %> </li>
+                          <li> Redondear: <%= orador.reacciones.redondeando.length %> </li>
+                    </ul>
+                </li>
+            <% }) %>
+        </ul>
+        <p>Action items:</p>
+        <ul>
+            <% tema.actionItems.forEach(({ actionItem }) => { %>
+                <li> <%= actionItem.descripcion %>. Owners: <%= actionItem.owners %> </li>
+            <% }) %>
+        </ul>
+      <% }) %> 
+    `, {
+      fecha,
+      temas,
+      temasAListar,
+      reaccionesATema,
+      tiempoHablando,
+    }, 'utf8')
+  );
+};
+
+export default componerMailResumen;
