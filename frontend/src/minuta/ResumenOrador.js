d416caeaab34a7c440b7f363792e960a6772152c
Lautaro Alonso Urtiaga <lautaro.urtiaga@10pines.com>
2020-06-24 21:46:11 -0300

    Cuando alguien escribe el resumen de de la minuta, si editó el texto, el boton de cancelar se convierte en "descartar cambios"

--- frontend/src/minuta/ResumenOrador.js
+++ frontend/src/minuta/ResumenOrador.js
@@ -8,7 +8,8 @@ import {ResumenInput,
 
 export const ResumenOrador = ({exposicion, onDiscard, onSave})=>{
 
-    let [resumen, setResumen] = useState('')
+    const [resumen, setResumen] = useState('')
+    const [fueEditado, setFueEditado] = useState(false)
 
     useEffect(()=>{
         if(exposicion) setResumen(exposicion.resumen || "")
@@ -28,19 +29,25 @@ export const ResumenOrador = ({exposicion, onDiscard, onSave})=>{
     const onDiscardSummary = ()=>{
         onDiscard()
         setResumen('')
+        setFueEditado(false)
     }
 
     const onGuardarResumen = ()=>{
         onSave(resumen)
         setResumen('')
+        setFueEditado(false)
     }
 
+    const onValueChange = (e)=> {
+        setResumen(e.target.value)
+        setFueEditado(true)
+    };
     return(
         <ContenedorResumen key="container">
             <TituloDeResumen  disabled={isButtonDisabled()}>{TitleText()}</TituloDeResumen>
-            <ResumenInput value={resumen} onChange={(e)=>setResumen(e.target.value)} disabled={isButtonDisabled()} rows={10}/>
+            <ResumenInput value={resumen} onChange={onValueChange} disabled={isButtonDisabled()} rows={10}/>
             <BotonesDeResumen>
-            <TextButton onClick={onDiscardSummary} disabled={isButtonDisabled()}>Descartar cambios</TextButton>
+            <TextButton onClick={onDiscardSummary} disabled={isButtonDisabled()}>{fueEditado? "Descartar cambios" : "Cancelar"}</TextButton>
             <ThemedButton onClick={onGuardarResumen} disabled={isButtonDisabled()}>Guardar</ThemedButton>
             </BotonesDeResumen>
         </ContenedorResumen>
