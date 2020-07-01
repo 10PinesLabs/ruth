f9ec76e6cf639e20a48d8f2c9f20de8596686d4b
Ignacio Pugliese <pugliese.ignacio@gmail.com>
2020-07-01 10:36:15 -0300

    FIx:scroll en navegacion (#193)
    * -se quita el scroll
    -se mueve el border de seleccion a la izquierda
    -se quita el icono gigante y se hace parte del texto
    -se quita el paddign del texto para una mejor navegacion
    -navegacion mas responsive
    * -la scrollbar no molesta mas
    
    Co-authored-by: ignacio pugliese <ignacio.pugliese@10pines.com>

--- frontend/src/sidebar-reunion/SidebarElement.js
+++ frontend/src/sidebar-reunion/SidebarElement.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { SeleccionImagen, SeleccionContainer, TitulosSidebar } from './Sidebar.styled';
+import { SeleccionContainer, TitulosSidebar } from './Sidebar.styled';
 
-class SidebarElement extends React.Component {
-  render() {
-    return (
-        <SeleccionContainer isActive={this.props.isActive}>
-          <SeleccionImagen>
-            <FontAwesomeIcon icon={this.props.icon} size="5x" />
-          </SeleccionImagen>
-          <TitulosSidebar>{this.props.title}</TitulosSidebar>
-        </SeleccionContainer>
-    );
-  }
+const SidebarElement  = ({isActive, icon, title}) => {
+
+  return (
+      <SeleccionContainer isActive={isActive}>
+        <TitulosSidebar><FontAwesomeIcon icon={icon}  /> {title}</TitulosSidebar>
+      </SeleccionContainer>
+  );
+
 }
 
 export default SidebarElement;
