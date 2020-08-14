4e97b2da64e4a2491239e81f8cbaa4007657ba64
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-08-14 12:21:44 -0300

    Se quita el import de reunion eventos

--- frontend/src/ReduxWebSocketWrapper.js
+++ frontend/src/ReduxWebSocketWrapper.js
@@ -2,7 +2,6 @@
 import { useEffect, useState } from 'react';
 import { batch } from 'react-redux';
 import createStore from './store';
-import { reunionEventos } from "./store/reunion";
 
 function getWebSocket(lastEvent) {
   const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
