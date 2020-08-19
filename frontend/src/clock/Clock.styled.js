77bdeae885bb937db34421c158dadb440a4c1441
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-08-19 16:05:15 -0300

    Quito 1em a clock.syled

--- frontend/src/clock/Clock.styled.js
+++ frontend/src/clock/Clock.styled.js
@@ -4,7 +4,6 @@ import { font } from '../styles/theme';
 export const StyledClockContainer = styled.div(({ isInteractive }) => `
   color: ${isInteractive ? 'silver' : 'black'};
   font-family: ${font.h1};
-  font-size: 1em  ;
   width: 60px;
   text-align: center;
   margin-right: 2px;
