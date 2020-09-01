b4279a60aa620626c05d58204a22fea03ffd993c
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-09-01 16:10:11 -0300

    agrego la apiKey

--- backend/src/domain/reuniones/actualizadorMinutaRoots.js
+++ backend/src/domain/reuniones/actualizadorMinutaRoots.js
@@ -1,5 +1,5 @@
 
-const urlApiMinutaDeTema = (temaId) => `/api/v2/temas/${temaId}/temaDeMinuta`;
+const urlApiMinutaDeTema = (temaId) => `/api/v2/temas/${temaId}/temaDeMinuta?apiKey=${process.env.TEMAS_ROOTS_API_KEY}`;
 const actualizarTemaTratadoEnRoots = async (requester, tema) => {
   requester.patch(process.env.TEMAS_ROOTS_HOST + urlApiMinutaDeTema(tema.id), {
     fueTratado: true,
--- backend/src/domain/reuniones/actualizadorMinutaRoots.test.js
+++ backend/src/domain/reuniones/actualizadorMinutaRoots.test.js
@@ -11,7 +11,7 @@ const mockRequester = {
 describe('Al actualizar un tema en roots', () => {
   test('el primer argumento es la url de la api',async () => {
     await actualizarTemaTratadoEnRoots(mockRequester, mockTema);
-    expect(mockRequester.patch.mock.calls[0][0]).toBe(`${process.env.TEMAS_ROOTS_HOST}/api/v2/temas/4/temaDeMinuta`);
+    expect(mockRequester.patch.mock.calls[0][0]).toBe(`${process.env.TEMAS_ROOTS_HOST}/api/v2/temas/4/temaDeMinuta?apiKey=${process.env.TEMAS_ROOTS_API_KEY}`);
   });
 
   test('hace patch a que el tema fue tratado',async () => {
