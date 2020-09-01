549ccb6456f6beb534f3404c3d552fddccc4caa8
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-09-01 12:44:58 -0300

    async a actualizar

--- backend/src/domain/reuniones/actualizadorMinutaRoots.js
+++ backend/src/domain/reuniones/actualizadorMinutaRoots.js
@@ -1,6 +1,6 @@
 
 const urlApiMinutaDeTema = (temaId) => `/api/v1/temas/${temaId}/temaDeMinuta`;
-const actualizarTemaTratadoEnRoots = (requester, tema) => {
+const actualizarTemaTratadoEnRoots = async (requester, tema) => {
   requester.patch(process.env.TEMAS_ROOTS_HOST + urlApiMinutaDeTema(tema.id), {
     fueTratado: true,
   });
--- backend/src/domain/reuniones/actualizadorMinutaRoots.test.js
+++ backend/src/domain/reuniones/actualizadorMinutaRoots.test.js
@@ -10,12 +10,12 @@ const mockRequester = {
 
 describe('Al actualizar un tema en roots', () => {
   test('el primer argumento es la url de la api', () => {
-    actualizarTemaTratadoEnRoots(mockRequester, mockTema);
+    await actualizarTemaTratadoEnRoots(mockRequester, mockTema);
     expect(mockRequester.patch.mock.calls[0][0]).toBe(`${process.env.TEMAS_ROOTS_HOST}/api/v1/temas/4/temaDeMinuta`);
   });
 
   test('hace patch a que el tema fue tratado', () => {
-    actualizarTemaTratadoEnRoots(mockRequester, mockTema);
+    await actualizarTemaTratadoEnRoots(mockRequester, mockTema);
     expect(mockRequester.patch.mock.calls[0][1]).toEqual({ fueTratado: true });
   });
 });
