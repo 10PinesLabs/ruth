0368afa10df9490f37a169b59ab3393d7770ad36
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-09-01 11:11:59 -0300

    agrego la base de la url desde env

--- backend/src/domain/reuniones/actualizadorMinutaRoots.js
+++ backend/src/domain/reuniones/actualizadorMinutaRoots.js
@@ -1,7 +1,7 @@
 
 const urlApiMinutaDeTema = (temaId) => `/api/v1/temas/${temaId}/temaDeMinuta`;
 const actualizarTemaTratadoEnRoots = (requester, tema) => {
-  requester.patch(urlApiMinutaDeTema(tema.id), {
+  requester.patch(process.env.TEMAS_ROOTS_HOST + urlApiMinutaDeTema(tema.id), {
     fueTratado: true,
   });
 };
