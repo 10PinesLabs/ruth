4fd6cceac9c422f556c43e520180085679cdbf9c
Angel Yan <angelyan96@gmail.com>
2021-05-06 14:39:33 -0300

    Prueba

--- backend/src/domain/mail/mail.js
+++ backend/src/domain/mail/mail.js
@@ -18,7 +18,6 @@ async function enviarResumenPorMail(reunion, temas) {
     },
   });
 
-
   await transporter.sendMail({
     from: `${process.env.MAIL_SENDER_NAME} <${process.env.MAIL_SENDER_ADRESS}>`,
     to: process.env.MAIL_DESTINATION,
