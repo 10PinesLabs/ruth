d08b5027e6c1696c240af5c9cc3d0cf4b68f192a
NicoleBlasco <nicole.blasco@10pines.com>
2021-07-01 12:55:51 -0300

    Agrega para cada tipo de reunion su correspondiente tab

--- frontend/src/empezar-reunion/ReunionesCerradas.js
+++ frontend/src/empezar-reunion/ReunionesCerradas.js
@@ -7,26 +7,24 @@ import {useHistory} from "react-router-dom";
 import {ButtonReunionCerrada} from "../components/Button.styled";
 
 const FilaReunion = ({reunion,history}) => {
-
-   /* const onClick = () => {
+    const onClick = () => {
         history.push(`/${reunion.id}/`)
-    }*/
-
+    }
     return <TableRow>
         <StyledTableCell >{reunion.temas.length > 1 ? "Reunion de Roots" : reunion.nombre || reunion.temas[0].titulo}</StyledTableCell>
         <StyledTableCell >{reunion.temas.length > 1 ? "Roots" : reunion.temas[0].autor}</StyledTableCell>
         <StyledTableCell >{"2021-07-01"}</StyledTableCell>
         <StyledTableCell >
-            <ButtonReunionCerrada className="boton-reunion-cerrada" /*onClick={onClick}*/>
+            <ButtonReunionCerrada className="boton-reunion-cerrada" onClick={onClick}>
                 Ver
             </ButtonReunionCerrada>
-            <ButtonReunionCerrada className="boton-reunion-cerrada" /*onClick={onClick}*/>
+            <ButtonReunionCerrada className="boton-reunion-cerrada" onClick={onClick}>
                 Ver Minuta
             </ButtonReunionCerrada>
-            <ButtonReunionCerrada className="boton-reunion-cerrada" /*onClick={onClick}*/>
+            <ButtonReunionCerrada className="boton-reunion-cerrada" onClick={onClick}>
                 Reenviar mail de minuta
             </ButtonReunionCerrada>
-            <ButtonReunionCerrada className="boton-reunion-cerrada" /*onClick={onClick}*/>
+            <ButtonReunionCerrada className="boton-reunion-cerrada" onClick={onClick}>
                 Reenviar recordatorios de slack
             </ButtonReunionCerrada>
         </StyledTableCell>
@@ -40,7 +38,7 @@ export const ReunionCerradas = () => {
     const history = useHistory();
     const [reuniones,setReuniones] = useState()
     useEffect(() => {
-        backend.obtenerReunionesCerradas()
+        backend.obtenerReunionesAbiertas()
             .then( ({reuniones}) => setReuniones(reuniones))
     }, []);
 
--- frontend/src/empezar-reunion/Tab.js
+++ frontend/src/empezar-reunion/Tab.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
-import {useTheme} from '@material-ui/core/styles';
+import {useTheme } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import {ReunionesActivasWrapper, ReunionesActivasTitle} from "./EmpezarReunion.styled";
